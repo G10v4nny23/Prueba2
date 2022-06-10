705acafcf21e50238db9c9880f35bb16d7ecf6a6
Giovanny <vi.riverazambrano@duocuc.cl>
2022-06-10 13:35:54 -0400

    COMMIT

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function getBasePath(service) {
+    return "https://chilealerta.com/api/query/?user=demo&select=" + service;
+}
+
+function createRowDisaster(dato1, dato2, dato3, dato4, dato5) {
+    return '<tr><td>' + dato1 + '</td><td>' + dato2 + '</td><td>' + dato3 + '</td><td>' + dato4 + '</td><td>' + dato5 + '</td></tr>';
+}
+
+function createRowDisasterUrl(dato1, dato2, dato3, dato4, dato5) {
+    return '<tr><td>' + dato1 + '</td><td>' + dato2 + '</td><td>' + dato3 + '</td><td>' + dato4 + '</td><td><a href=' + dato5 + '>Mas Info</a></td></tr>';
+}
+
+function callInfo(dato1) {
+    return dato1;
+}
